eb12803f5b51bd02466e74142af37bfa952bd09b
biaomarcos <biao.marcosvinicius@gmail.com>
2015-07-30 10:32:52 -0300

    head

--- telaCadastroBeneficiario.php
+++ telaCadastroBeneficiario.php
@@ -4,7 +4,57 @@
 <?php include("head.html")?>
     <body>
         <center> <img src="imagem/seadUfrb.png" width="700" height="300"> </center>
-        <?php include("cabecario.html")?>
+       <ul class="nav nav-tabs">
+        <!--Dropdown para bolsa estagio-->
+        <li role="presentation" class="dropdown">
+             <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+               Bolsa Estagio <span class="caret"></span>
+             </a>
+             <ul class="dropdown-menu">
+                <li role="presentation"><a href="#">Cadastrar beneficiario</a></li>
+                <li role="presentation"><a href="#">Consultar beneficiario</a></li>
+                <li role="presentation"><a href="#">Solicitar bolsa</a></li>
+                <li role="presentation"><a href="#">Consultar solicitação de bolsa</a></li>
+            </ul>
+        </li>  
+        <!-- Fim do dropdown bolsa estagio e inicio do drop bolsa pesquisa -->
+        <li role="presentation" class="dropdown">
+             <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+               Bolsa Pesquisa <span class="caret"></span>
+             </a>
+             <ul class="dropdown-menu">
+                <li role="presentation"><a href="#">Cadastrar beneficiario</a></li>
+                <li role="presentation"><a href="#">Consultar beneficiario</a></li>
+                <li role="presentation"><a href="#">Solicitar bolsa</a></li>
+                <li role="presentation"><a href="#">Consultar solicitação de bolsa</a></li>
+            </ul>
+        </li>
+  
+        <li role="presentation" class="dropdown">
+             <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+               Diarias <span class="caret"></span>
+             </a>
+             <ul class="dropdown-menu">
+                <li role="presentation"><a href="#">Cadastrar beneficiario</a></li>
+                <li role="presentation"><a href="#">Consultar beneficiario</a></li>
+                <li role="presentation"><a href="#">Solicitar bolsa</a></li>
+                <li role="presentation"><a href="#">Consultar solicitação de bolsa</a></li>
+            </ul>
+        </li>
+        
+        <li role="presentation" class="dropdown">
+             <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+               Passagens <span class="caret"></span>
+             </a>
+             <ul class="dropdown-menu">
+                <li role="presentation"><a href="#">Cadastrar beneficiario</a></li>
+                <li role="presentation"><a href="#">Consultar beneficiario</a></li>
+                <li role="presentation"><a href="#">Solicitar bolsa</a></li>
+                <li role="presentation"><a href="#">Consultar solicitação de bolsa</a></li>
+            </ul>
+        </li>
+  
+      </ul>
         
         
         
