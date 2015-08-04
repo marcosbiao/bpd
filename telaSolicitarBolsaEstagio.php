dcf3a96b960fd48acb7f1e9625beec2c815dec83
biaomarcos <biao.marcosvinicius@gmail.com>
2015-08-04 17:23:16 -0300

    TelaSolicitaBolsaEstagio

--- telaSolicitarBolsaEstagio.php
+++ telaSolicitarBolsaEstagio.php
@@ -1,10 +1,26 @@
 <html lang="pt">
-
+    <?php 
+    include("banco/banco.php");
+    $query = "select nome from programa";
+    $rs = Select ($query);
+    $row = mysql_fetch_assoc($rs);
+    ?>
 
     <?php include("head.html");  ?>
     <body>
        <?php  include("cabecario.html");  ?>
-
+        <h3>Solicitação de Estagio</h3>
+        
+    <center>
+        <div>
+            Projetos: 
+            <select id="programa" name="programa">
+                <?php while($row = mysql_fetch_assoc($rs)) { 
+                echo "<option value= {$rs['idPrograma']} > {$rs['nome']} </option> ";
+                } ?>
+            </select>
+        </div>
+    </center>
         
         
         <?php include("script.html"); ?>
