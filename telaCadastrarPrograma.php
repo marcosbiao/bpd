fc10f5c8f24bfdaaf20afd7037103bb250052241
biaomarcos <biao.marcosvinicius@gmail.com>
2015-08-04 15:57:43 -0300

    telaCadatrarPrograma

---
+++ telaCadastrarPrograma.php
@@ -0,0 +1,28 @@
+<html lang="pt">
+
+
+    <?php include("head.html");  ?>
+    <body>
+       <?php  include("cabecario.html");  ?>
+        
+        <div class="container panel panel-default">
+			</br></br>
+                        <form action = "fachadaCadastroBeneficiario.php" method = "POST">
+				<div class="panel panel-default jumbotron">	
+					<div id="vpav"></div>
+					<h3>Dados obrigatorios</h3>
+					<div class="input-group">
+	  					<span class="input-group-addon" id="sizing-addon2">Nome</span>
+	  					<input type="text" class="form-control"  id="textFieldCpf" name="textFieldNome" placeholder="Digite o nome do programa" aria-describedby="sizing-addon2">
+					</div>
+                                        <div>
+                                            <input type="submit" id = "bm" class="btn btn-default topElementos" value="Salvar" />
+                                        </div>
+                                </div>
+                        </form>
+        </div>
+        
+        <?php include("script.html"); ?>
+        <?php include("foot.html"); ?>    
+    </body>
+</html>
