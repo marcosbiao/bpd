21302a284d8e6763a7077c618bf9d93947c1c6a2
biaomarcos <biao.marcosvinicius@gmail.com>
2015-08-25 09:03:54 -0300

    TelaLogin

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<?php include("head.html"); ?>
+
+    <body>
+        <center> <img src="imagem/seadUfrb.png" width="700" height="300"> </center>
+        <div>
+            <form action="" method="POST">
+                <center>
+                    <label>Login</label>
+                    <input type="text" name="login" id="login">
+                    <label>Senha</label>
+                    <input type="text" name="senha" id="senha">
+                </center>
+            </form>
+        </div>
+    </body>
+    
+</html>
