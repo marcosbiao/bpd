dddebe4bd9b6ce59c7d0ab1398fa8206e5dbd265
biaomarcos <biao.marcosvinicius@gmail.com>
2015-08-25 09:12:56 -0300

    TelaLogin

--- login.php
+++ login.php
@@ -6,12 +6,19 @@
     <body>
         <center> <img src="imagem/seadUfrb.png" width="700" height="300"> </center>
         <div>
-            <form action="" method="POST">
+            <form action="validadorLogin.php" method="POST">
                 <center>
-                    <label>Login</label>
-                    <input type="text" name="login" id="login">
-                    <label>Senha</label>
-                    <input type="text" name="senha" id="senha">
+                    <div>
+                        <label>Login</label>
+                        <input type="text" name="login" id="login">
+                    </div>
+                    <div>
+                        <label>Senha</label>
+                        <input type="password" name="senha" id="senha">
+                    </div>
+                    <div>
+                        <input type="submit" value="Login">
+                    </div>
                 </center>
             </form>
         </div>
---
+++ validadorLogin.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<?php include("head.html"); ?>
+    <body>
+        
+    </body>
+</html>
