bdabdbeff1628ed86f04aba7fb00ae632d298c3a
biaomarcos <biao.marcosvinicius@gmail.com>
2015-08-25 14:21:49 -0300

    correção em niome de classes

---
+++ home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+   <?php include("head.html"); ?>
+     
+  <body>
+    
+    <?php  include("cabecario.html");  ?>
+    <?php include("script.html"); ?>
+    <?php include("foot.html"); ?>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html lang="pt">
 
-   <?php include("head.html"); ?>
-     
-  <body>
+<?php include("head.html"); ?>
+
+    <body>
+        <center> <img src="imagem/seadUfrb.png" width="700" height="300"> </center>
+        <div>
+            <form action="validadorLogin.php" method="POST">
+                <center>
+                    <div>
+                        <label>Login</label>
+                        <input type="text" name="login" id="login">
+                    </div>
+                    <div>
+                        <label>Senha</label>
+                        <input type="password" name="senha" id="senha">
+                    </div>
+                    <div>
+                        <input type="submit" value="Login">
+                    </div>
+                </center>
+            </form>
+        </div>
+    </body>
     
-    <?php  include("cabecario.html");  ?>
-    <?php include("script.html"); ?>
-    <?php include("foot.html"); ?>
-  </body>
 </html>
