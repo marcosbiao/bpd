f9164efab41566bebdc2bde252bb0ce478606054
biaomarcos <biao.marcosvinicius@gmail.com>
2015-09-01 16:53:34 -0300

    telaCadastraUsuario

---
+++ fachadaCadastraUsuario.php
@@ -0,0 +1,25 @@
+<?php
+
+  // incluindo o arquivo do banco de dados
+                    include ("banco/banco.php");
+                    // instanciando a classe do banco
+                    //$b = new database();
+                    // pegando o nome e tirando os espaços no inicio e no fim com a funcao "trim"
+                    
+                    $login = trim($_POST["textFieldLogin"]);
+                    $senha = trim($_POST["textFieldSenha"]);
+                    $nivel = trim($_POST["nivel"]);
+                    
+                    //$data2 = explode("-" or "/" or "." ,$data); 
+                    //$data = implode("",$data2);
+                    // chamando a função query da classe banco para adicionar ao banco de dados
+                    $b = "INSERT INTO usuario (login,senha,nivel) VALUES ('$login','$senha',$nivel)";
+                    echo $b;
+                    noQuery($b);
+?>
+
+<script>
+    
+    window.location = "telaPrincipal.php"
+    
+</script>
---
+++ telaCadastraUsuario.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+    <?php include("head.php"); ?>
+     
+  <body>
+    
+    <?php include("escolheCabecario.php");?>
+      
+      <div class="container panel panel-default">
+			</br></br>
+                        <form action = "fachadaCadastraUsuario.php" method = "POST">
+				<div class="panel panel-default jumbotron">	
+					<div id="vpav"></div>
+					<h3>Dados obrigatorios</h3>
+					<div class="input-group">
+	  					<span class="input-group-addon" id="sizing-addon2">Login</span>
+	  					<input type="text" class="form-control"  id="textFieldLogin" name="textFieldLogin" placeholder="Digite o login do usuario" aria-describedby="sizing-addon2">
+					</div>
+                                        
+                                        <div class="input-group">
+	  					<span class="input-group-addon" id="sizing-addon2">Senha</span>
+	  					<input type="text" class="form-control"  id="textFieldSenha" name="textFieldSenha" placeholder="Digite a senha do usuario" aria-describedby="sizing-addon2">
+					</div>
+                                        
+                                         <div class="input-group">
+                                            <span class="input-group-addon" id="sizing-addon2">Carga Horaria</span>
+                                            <select id="nivel" name="nivel">
+                                                <option value="">---- Selecione ----</option>
+                                                <option value="1">Administrador Geral</option>
+                                                <option value="2">Coordenador de Curso</option>
+                                            </select>
+                                         </div>
+                                        
+                                        <div>
+                                            <input type="submit" id = "bm" class="btn btn-default topElementos" value="Cadastrar" />
+                                        </div>
+                                </div>
+                        </form>
+        </div>
+      
+    <?php include("script.html"); ?>
+    <?php include("foot.html"); ?>
+  </body>
+</html>
